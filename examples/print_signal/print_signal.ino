ddaa80737e6375e32a9777a50ebadf71caa0a2c9
Sven Forsberg <sven.johnsson@gmail.com>
2016-09-11 21:26:35 +0200

    Added example code

---
+++ examples/print_signal/print_signal.ino
@@ -0,0 +1,40 @@
+#include "SoundInput.h"
+
+/*
+Sample program to print incoming ADC samples to serial port!
+*/
+
+int buf[100],buf_ct=0,lock=0;
+SoundInput Sound=SoundInput(&putBuffer);
+
+void setup()
+{
+  Sound.begin();
+  Serial.begin(115200);
+}
+
+void putBuffer(int val)
+{
+  if(lock==0){
+  buf[buf_ct]=val;
+  buf_ct++;
+  if(buf_ct>100) buf_ct=0;
+  }
+}
+
+void loop()
+{
+  if(buf_ct==99)
+  {
+    int i;
+    lock=1;
+    for(i=0;i<100;i++)
+    {
+      Serial.println(buf[i]);
+    }
+    lock=0;
+  }
+}
+
+
+
